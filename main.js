82b2d6ce96d8346405ae49dda454f1a3be308661
Tundra Fizz <MageLeif@Yahoo.com>
2018-03-05 17:28:10 -0800

    Update files

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, ipcMain, clipboard} = require("electron");
+const {app, BrowserWindow, Tray, Menu, ipcMain, clipboard} = require("electron");
 const path  = require("path");
 const url   = require("url");
 var fs      = require("fs");      // File system
@@ -8,15 +8,12 @@ var m       = require("./");      // C++ module
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var win;
-
-// function KeyDownFn(evt){
-//   if(evt.keyCode == 73 && evt.ctrlKey && evt.shiftKey)
-//     evt.preventDefault();
-// }
+var win  = null;
+var tray = null;
+var quit = false;
 
 function createWindow(){
-  // Create the browser window.
+  // Create the browser window
   win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -24,29 +21,91 @@ function createWindow(){
     icon: path.join(__dirname, "icon64x64.png")
   });
 
-  win.once("ready-to-show", () => {
-    win.minimize();
-    // win.show();
+  tray = new Tray(path.join(__dirname, "icon64x64.png"));
+
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      "label": "Item 1",
+      "type" : "radio",
+      "icon" : path.join(__dirname, "icon64x64.png")
+    },
+    {
+      "label": "Item 2",
+      "submenu": [
+        {"label": "submenu 1"},
+        {"label": "submenu 2"},
+      ]
+    },
+    {
+      "label": "Item 3",
+      "type" : "radio",
+      "checked": true
+    },
+    {
+      "label": "Toggle Tools",
+      "accelerator": "Ctrl+I",
+      "click": function(){
+        win.webContents.toggleDevTools();
+      }
+    },
+    {
+      "label": "Quit Frost Frame",
+      "click": function(){
+        quit = true;
+        win.close();
+      }
+    }
+  ]);
+
+  tray.setToolTip("App icon tooltip!");
+  tray.setContextMenu(contextMenu);
+
+  tray.on("click", () => {
+    console.log("Single click");
+    win.showInactive();
   });
 
-  // and load the index.html of the app.
+  tray.on("double-click", () => {
+    console.log("Double click");
+    win.focus();
+  });
+
+  win.once("ready-to-show", () => {
+    var loggedIn = false; // DEBUG VARIABLE
+
+    if(loggedIn)
+      win.hide();
+    else
+      win.show();
+  });
+
+  // Load the index.html of the app
   win.loadURL(url.format({
     pathname: path.join(__dirname, "index.html"),
     protocol: "file:",
     slashes: true
   }));
 
-  // function KeyDownFn(evt){
-  //   if(evt.keyCode == 73 && evt.ctrlKey && evt.shiftKey)
-  //     evt.preventDefault();
-  // }
+  // When the window is going to be minimized
+  win.on("minimize",function(event){
+    event.preventDefault();
+    win.hide();
+  });
 
-  // Emitted when the window is closed.
+  // When the window is about to be closed
+  win.on("close", function (event){
+    if(quit == false){
+      event.preventDefault();
+      win.hide();
+    }
+  });
+
+  // When the window is actually closed
   win.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    win = null;
   });
 }
 
@@ -54,8 +113,6 @@ function createWindow(){
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
-
-// win.webContents.openDevTools(); // Open the DevTools
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
@@ -69,7 +126,7 @@ app.on("window-all-closed", () => {
 app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (win === null){
     createWindow()
   }
 });
@@ -81,6 +138,8 @@ ipcMain.on("message", (event, msg) => {
 });
 
 app.on("test", (arg) => {
+  win.minimize();
+
   m.doAsyncStuff(123, 5, true, function(result, error){
     console.log(`Sending ${result} to the server`);
 
